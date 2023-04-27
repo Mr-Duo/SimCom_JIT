fad32ba6c2db70c44ad4b40f75dbd5bb8d0e5653
Jirachiii <trangdy1112@gmail.com>
2023-04-27 13:32:37 +0700

    Create gitignore

--- Com/run_com.sh
+++
@@ -1,9 +0,0 @@
-python main.py  -train -project openstack -do_valid
-
-python main.py  -train -project platform -do_valid
-
-python main.py  -train -project gerrit -do_valid
-
-python main.py  -train -project go -do_valid
-
-
