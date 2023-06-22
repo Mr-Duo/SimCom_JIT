770b0d29b1c23fbe5954c117029ef2eea0f504e5
manhlamabc123 <tranducmanh12092001@gmail.com>
2023-06-22 20:20:50 +0700

    Update .gitignore

--- combine.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-python combination.py -project $1
--- sim.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-cd Sim
-
-python sim_model.py \
-    -project openstack \
-    -train_data "../data/hand_crafted_features/openstack/k_train.csv" \
-    -test_data "../data/hand_crafted_features/openstack/k_test.csv"
