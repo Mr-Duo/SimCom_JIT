79bc573f414c41320d647464d96bce4c9eef21e1
manhlamabc123 <tranducmanh12092001@gmail.com>
2023-10-17 14:42:32 +0700

    Update bash script

---
+++ run_all_part_1.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+bash run_com.sh phaser part_3_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
+
+bash run_com.sh cpython part_3_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
+
+bash run_com.sh salt part_3_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
+
+bash run_com.sh swift part_3_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
---
+++ run_all_part_2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+bash run_com.sh yuzu part_3_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
+
+bash run_com.sh elasticsearch part_3_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
+
+bash run_com.sh php-src part_3_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
+
+bash run_com.sh shardingsphere part_3_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
+
+bash run_com.sh tensorflow part_3_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
