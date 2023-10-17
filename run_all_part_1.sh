d804c45be696dc6ba404ef93d3e1311cef5f84a1
manhlamabc123 <tranducmanh12092001@gmail.com>
2023-10-17 16:48:48 +0700

    Update scripts

--- run_all_part_1.sh
+++ run_all_part_1.sh
@@ -1,9 +1,13 @@
 #! /bin/bash
 
-bash run_com.sh phaser part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
+bash run_com.sh flink part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
 
-bash run_com.sh cpython part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
+bash run_com.sh jenkins part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
 
-bash run_com.sh salt part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
+bash run_com.sh numpy part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
 
-bash run_com.sh swift part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
+bash run_com.sh pandas part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
+
+bash run_com.sh scikit-learn part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
+
+bash run_com.sh TDengine part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
--- run_all_part_2.sh
+++ run_all_part_2.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-bash run_com.sh yuzu part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
+bash run_com.sh Ghost part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
 
-bash run_com.sh elasticsearch part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
+bash run_com.sh scipy part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
 
-bash run_com.sh php-src part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
+bash run_com.sh presto part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
 
-bash run_com.sh shardingsphere part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
+bash run_com.sh openssl part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
 
-bash run_com.sh tensorflow part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
+bash run_com.sh mpv part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
