362281656d3a4101d8f7e87cf9b7f997967bd6b3
manhlamabc123 <tranducmanh12092001@gmail.com>
2023-10-20 13:46:09 +0700

    Update scripts

--- run_all_part_1.sh
+++ run_all_part_1.sh
@@ -1,13 +1,11 @@
 #! /bin/bash
 
-bash run_com.sh flink part_3_part_4 part_5 /media/aiotlab3/27934be5-a11a-44ba-8b28-750d135bc3b3/RISE/Manh/splited-tandataset 30
-
-bash run_com.sh jenkins part_3_part_4 part_5 /media/aiotlab3/27934be5-a11a-44ba-8b28-750d135bc3b3/RISE/Manh/splited-tandataset 30
-
-bash run_com.sh numpy part_3_part_4 part_5 /media/aiotlab3/27934be5-a11a-44ba-8b28-750d135bc3b3/RISE/Manh/splited-tandataset 30
-
-bash run_com.sh pandas part_3_part_4 part_5 /media/aiotlab3/27934be5-a11a-44ba-8b28-750d135bc3b3/RISE/Manh/splited-tandataset 30
-
-bash run_com.sh scikit-learn part_3_part_4 part_5 /media/aiotlab3/27934be5-a11a-44ba-8b28-750d135bc3b3/RISE/Manh/splited-tandataset 30
-
-bash run_com.sh TDengine part_3_part_4 part_5 /media/aiotlab3/27934be5-a11a-44ba-8b28-750d135bc3b3/RISE/Manh/splited-tandataset 30
+# bash run_com.sh phaser part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
+bash run_com.sh elasticsearch part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
+bash run_com.sh php-src part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
+# bash run_com.sh yuzu part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
+bash run_com.sh swift part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
+# bash run_com.sh cpython part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
+bash run_com.sh salt part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
+# bash run_com.sh shardingsphere part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
+bash run_com.sh tensorflow part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 30
