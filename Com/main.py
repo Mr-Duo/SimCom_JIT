e295ee55bce74290f97acdf58a6fb815eb3aa33b
manhtdd <manh.td120901@gmail.com>
2024-01-08 16:26:03 +0700

    Update seed

--- Com/main.py
+++ Com/main.py
@@ -2,7 +2,20 @@ import argparse
 from train import train_model
 from evaluation import evaluation_model
 from preprocess import preprocess_data
-import torch
+import torch, random, os
+import numpy as np 
+
+def seed_torch(seed=42):
+	random.seed(seed)
+	os.environ['PYTHONHASHSEED'] = str(seed) 
+	np.random.seed(seed)
+	torch.manual_seed(seed)
+	torch.cuda.manual_seed(seed)
+	#torch.cuda.manual_seed_all(seed) # if you are using multi-GPU.
+	torch.backends.cudnn.benchmark = False
+	torch.backends.cudnn.deterministic = True
+
+seed_torch()
 
 def read_args():
     parser = argparse.ArgumentParser()
