93b3e38e970d14157c90ad333b7de1439552160e
manhtdd <manh.td120901@gmail.com>
2024-01-12 16:44:19 +0700

    Update scripts

--- bash-script/deepjit_test.sh
+++ bash-script/deepjit_test.sh
@@ -6,5 +6,4 @@ python main.py -predict \
     -project $1_$2 \
     -predict_data "$5/$1/commits/deepjit_$1_$3.pkl" \
     -dictionary_data "$4/$1/commits/$1_$2_train_dict.pkl" \
-    -load_model "model/$1_$2/deepjit_best_model.pt" \
-    -num_epochs $5
+    -load_model "model/$1_$2/deepjit_best_model.pt"
