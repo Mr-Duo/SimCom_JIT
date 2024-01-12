aad7134a3f946095d5906e737e0e5d2f2bfa09cf
manhtdd <manh.td120901@gmail.com>
2024-01-12 16:15:02 +0700

    Update scripts

--- bash-script/run_deepjit.sh
+++ bash-script/run_deepjit.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-bash deepjit.sh c part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/crawling/cross-lang /data/gpfs/projects/punim1928/RISE/JITDP/crawling/single-lang 1
+cd bash-script && bash deepjit.sh c part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/crawling/cross-lang /data/gpfs/projects/punim1928/RISE/JITDP/crawling/single-lang 1
