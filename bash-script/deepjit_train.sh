8948d2a9c6707ba366f27e509513e48564752df0
manhtdd <manh.td120901@gmail.com>
2024-01-12 16:16:17 +0700

    Update scripts

--- bash-script/deepjit_train.sh
+++ bash-script/deepjit_train.sh
@@ -5,5 +5,5 @@ cd ../Com
 python main.py -train \
     -project $1_$2 \
     -train_data "$3/$1/commits/deepjit_$1_$2_train.pkl" \
-    -dictionary_data "$3/$1/commits/$1_$2_dict_train.pkl" \
+    -dictionary_data "$3/$1/commits/$1_$2_train_dict.pkl" \
     -num_epochs $4
