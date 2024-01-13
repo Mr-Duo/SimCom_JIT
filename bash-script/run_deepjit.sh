f605d7865702998f1c16ed9e31d3076eb35b9e95
manhtdd <manh.td120901@gmail.com>
2024-01-13 08:42:13 +0700

    Update scripts

--- bash-script/run_deepjit.sh
+++ bash-script/run_deepjit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-bash deepjit.sh java part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/crawling/cross-lang /data/gpfs/projects/punim1928/RISE/JITDP/crawling/single-lang 1
-bash deepjit.sh python part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/crawling/cross-lang /data/gpfs/projects/punim1928/RISE/JITDP/crawling/single-lang 1
-bash deepjit.sh c part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/crawling/cross-lang /data/gpfs/projects/punim1928/RISE/JITDP/crawling/single-lang 1
-bash deepjit.sh cpp part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/crawling/cross-lang /data/gpfs/projects/punim1928/RISE/JITDP/crawling/single-lang 1
-bash deepjit.sh javscript part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/crawling/cross-lang /data/gpfs/projects/punim1928/RISE/JITDP/crawling/single-lang 1
+bash deepjit.sh c part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/crawling/cross-lang /data/gpfs/projects/punim1928/RISE/JITDP/crawling/single-lang 30
+bash deepjit.sh cpp part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/crawling/cross-lang /data/gpfs/projects/punim1928/RISE/JITDP/crawling/single-lang 30
+bash deepjit.sh java part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/crawling/cross-lang /data/gpfs/projects/punim1928/RISE/JITDP/crawling/single-lang 30
+bash deepjit.sh javascript part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/crawling/cross-lang /data/gpfs/projects/punim1928/RISE/JITDP/crawling/single-lang 30
+bash deepjit.sh python part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/crawling/cross-lang /data/gpfs/projects/punim1928/RISE/JITDP/crawling/single-lang 30
