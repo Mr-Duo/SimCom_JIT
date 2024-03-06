d49d70cd79c6ed2a06f99b2b2b3e599c5962cf1b
Mr-Duo <103053799+Mr-Duo@users.noreply.github.com>
2024-03-06 11:47:46 +0700

    Update preprocess.py

--- Com/preprocess.py
+++ Com/preprocess.py
@@ -72,16 +72,16 @@ def preprocess_data(params):
     if params.train is True:
         # Load train data
         train_data = pickle.load(open(params.train_data, 'rb'))
-        ids, messages, codes, labels = train_data
+        ids, labels, messages, codes = train_data
     
     elif params.predict is True:
         # Load predict data
         predict_data = pickle.load(open(params.predict_data, 'rb'))
-        ids, messages, codes, labels = predict_data
+        ids, labels, messages, codes = predict_data
 
     if params.do_valid is True:
         val_data = pickle.load(open(params.test_data, 'rb'))
-        val_ids, val_messages, val_codes, val_labels = val_data
+        val_ids, val_labels, val_messages, val_codes = val_data
 
     dictionary = pickle.load(open(params.dictionary_data, 'rb'))   
     dict_msg, dict_code = dictionary
